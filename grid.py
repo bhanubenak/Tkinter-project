cd52deac0bfe828f7a3bfb9650a3600271ec1d12
bhanubenak <58963477+bhanubenak@users.noreply.github.com>
2020-10-06 19:16:42 +0530

    Update grid.py

--- grid.py
+++ grid.py
@@ -2,15 +2,13 @@ from tkinter import *
 
 root = Tk()
 
-# creating a label widget
-mylabel1 = Label(root, text="Hello world")
-mylabel2 = Label(root, text="My Name is BHANU PRAKASH")
-mylabel3 = Label(root, text=" ")
+# Creating a Label Widget
+myLabel1 = Label(root, text="Hello World!").grid(row=0, column=0)
+myLabel2 = Label(root, text="My Name is Bhanu prakash").grid(row=1, column=5)
+# Shoving it onto the screen
 
-# showing into the screen
-mylabel1.grid(row=0, column=0)
-mylabel2.grid(row=0, column=0)
-mylabel3.grid(row=0, column=0)
+#myLabel1.grid(row=0, column=0)
+#myLabel2.grid(row=1, column=5)
 
 
 root.mainloop()
