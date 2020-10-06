c2f1ca06a2630ec323979bc72674ac7c8e76b0e3
bhanubenak <58963477+bhanubenak@users.noreply.github.com>
2020-10-06 19:14:06 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,12 +1,23 @@
+
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import filedialog
 
 root = Tk()
-root.title("born to shine!!")
-
-root.filename = filedialog.askopenfilename(initialdir="C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\",
-                                           title="Select a file", fileypes=(("png files", "*.png"), ("all files", "*.*")))
+root.title('Image Viewer')
 
 
-mainloop()
+def open():
+    global my_image
+
+    root.filename = filedialog.askopenfilename(initialdir="C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\",
+                                               title="Select a file", filetypes=(("png files", "*.png"), ("all files", "*.*")))
+    my_label = Label(root, text=root.filename).pack()
+    my_image = ImageTk.PhotoImage(Image.open(root.filename))
+    my_image_label = Label(image=my_image).pack()
+
+
+my_btn = Button(root, text="Open File", command=open).pack()
+
+
+root.mainloop()
