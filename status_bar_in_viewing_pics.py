edaafc57ba2cc0697152f37e653cea476c7cb395
bhanubenak <58963477+bhanubenak@users.noreply.github.com>
2020-10-06 19:29:04 +0530

    Update status_bar_in_viewing_pics.py

--- status_bar_in_viewing_pics.py
+++ status_bar_in_viewing_pics.py
@@ -11,7 +11,7 @@ root.title("Born to Shine")
 my_img_1 = ImageTk.PhotoImage(Image.open(
     "C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\lee_quote.png"))
 my_img_2 = ImageTk.PhotoImage(Image.open(
-    "C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\me1.png"))
+    "C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\me.png"))
 my_img_3 = ImageTk.PhotoImage(Image.open(
     "C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\greenland.png"))
 my_img_4 = ImageTk.PhotoImage(Image.open(
@@ -19,7 +19,7 @@ my_img_4 = ImageTk.PhotoImage(Image.open(
 my_img_5 = ImageTk.PhotoImage(Image.open(
     "C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\sky.png"))
 my_img_6 = ImageTk.PhotoImage(Image.open(
-    "C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\wide.png"))
+    "C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\wide_angle.png"))
 
 image_list = [my_img_1, my_img_2, my_img_3, my_img_4, my_img_5, my_img_6]
 
@@ -90,96 +90,3 @@ button_forward.grid(row=1, column=2, pady=10)
 status.grid(row=2, column=0, columnspan=3, sticky=S+E)
 
 root.mainloop()
-
-'''
-from tkinter import *
-from PIL import ImageTk, Image
-
-root = Tk()
-root.title('sample stuff!!')
-# root.iconbitmap('c:/gui/codemy.ico')
-
-
-my_img1 = ImageTk.PhotoImage(Image.open(
-    "C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\lee_quote.png"))
-my_img2 = ImageTk.PhotoImage(Image.open(
-    "C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\me1.png"))
-my_img3 = ImageTk.PhotoImage(Image.open(
-    "C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\greenland.png"))
-my_img4 = ImageTk.PhotoImage(Image.open(
-    "C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\lee_logo.png"))
-my_img5 = ImageTk.PhotoImage(Image.open(
-    "C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\sky.png"))
-my_img6 = ImageTk.PhotoImage(Image.open(
-    "C:\\Users\\Admin\\Desktop\\tkinter(freecodecamp)\\images\\wide.png"))
-
-
-image_list = [my_img1, my_img2, my_img3, my_img4, my_img5, my_img6]
-
-status = Label(root, text="Image 1 of " + str(len(image_list)),
-               bd=1, relief=SUNKEN, anchor=E)
-
-my_label = Label(image=my_img1)
-my_label.grid(row=0, column=0, columnspan=3)
-
-
-def forward(image_number):
-    global my_label
-    global button_forward
-    global button_back
-
-    my_label.grid_forget()
-    my_label = Label(image=image_list[image_number-1])
-    button_forward = Button(
-        root, text=">>", command=lambda: forward(image_number+1))
-    button_back = Button(root, text="<<", command=lambda: back(image_number-1))
-
-    if image_number == 5:
-        button_forward = Button(root, text=">>", state=DISABLED)
-
-    my_label.grid(row=0, column=0, columnspan=3)
-    button_back.grid(row=1, column=0)
-    button_forward.grid(row=1, column=2)
-
-    status = Label(root, text="Image " + str(image_number) +
-                   " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
-    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
-
-
-def back(image_number):
-    global my_label
-    global button_forward
-    global button_back
-
-    my_label.grid_forget()
-    my_label = Label(image=image_list[image_number-1])
-    button_forward = Button(
-        root, text=">>", command=lambda: forward(image_number+1))
-    button_back = Button(root, text="<<", command=lambda: back(image_number-1))
-
-    if image_number == 1:
-        button_back = Button(root, text="<<", state=DISABLED)
-
-    my_label.grid(row=0, column=0, columnspan=3)
-    button_back.grid(row=1, column=0)
-    button_forward.grid(row=1, column=2)
-
-    # Update Status Bar
-    status = Label(root, text="Image " + str(image_number) +
-                   " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
-    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
-
-
-button_back = Button(root, text="<<", command=back, state=DISABLED)
-button_exit = Button(root, text="Exit Program", command=root.quit)
-button_forward = Button(root, text=">>", command=lambda: forward(2))
-
-
-button_back.grid(row=1, column=0)
-button_exit.grid(row=1, column=1)
-button_forward.grid(row=1, column=2, pady=10)
-status.grid(row=2, column=0, columnspan=3, sticky=W+E)
-
-
-root.mainloop()
-'''
